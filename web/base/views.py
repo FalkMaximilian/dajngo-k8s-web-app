223d7574c96309701c6e0360e36b51143cce5cd5
Maximilian Falk <falk.maximilian@outlook.com>
2023-10-11 20:18:04 +0200

    changed style

--- web/base/views.py
+++ web/base/views.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 def hallo_schatz(request):
-    return HttpResponse('<div style="width:100%;height:100%;text-align:center;justify-content:center;"><h1>Hallo Schatz! Liebe Grüße aus Spandau :*</h1></div>')
+    return HttpResponse('<div style="width:100%;height:100%;text-align:center;justify-content:center;padding-top:auto;padding-bottom:auto;"><h1>Hallo Schatz! Liebe Grüße aus Spandau :*</h1></div>')
