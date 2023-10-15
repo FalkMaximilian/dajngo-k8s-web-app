d49834149ac354f870935c540ed255ae8e9a283a
Maximilian Falk <maximilianfalk@Maximilians-MacBook-Air.local>
2023-10-15 21:23:29 +0200

    added redirect

--- web/base/views.py
+++ web/base/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 
 # Create your views here.
 
 def hallo_schatz(request):
-    return HttpResponse('<div style="width:100%;height:100%;text-align:center;justify-content:center;padding-top:auto;padding-bottom:auto;"><h1 style="margin-top:auto;margin-bottom:auto;">Hallo Schatz! Liebe Grüße aus Spandau :*</h1></div>')
+    return redirect('login-page')
 
 def login_page(request):
     return render(request, 'base/login_page.html')
