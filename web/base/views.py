2fc0e5041a1a8e2ab7a8f5cb8f9bab5ca83b5972
Maximilian Falk <maximilianfalk@Maximilians-MacBook-Air.local>
2023-10-15 21:03:25 +0200

    added bootstrap login page

--- web/base/views.py
+++ web/base/views.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 def hallo_schatz(request):
-    return HttpResponse('<div style="width:100%;height:100%;text-align:center;justify-content:center;padding-top:auto;padding-bottom:auto;"><h1 style="margin-top:auto;margin-bottom:auto;">Hallo Schatz! Liebe Grüße aus Spandau :*</h1></div>')
+    return render(request, 'templates/login_page.html')
