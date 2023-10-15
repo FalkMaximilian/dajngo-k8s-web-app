27d59fa9ad9efec47b8d735d9df6442da51d19c7
Maximilian Falk <maximilianfalk@Maximilians-MacBook-Air.local>
2023-10-15 21:11:21 +0200

    fix file structures

--- web/base/urls.py
+++ web/base/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import hallo_schatz
+from .views import hallo_schatz, login_page
 
 urlpatterns = [
     path('', hallo_schatz, name='hallo-schatz'),
+    path('login/', login_page, name='login-page')
 ]
--- web/base/views.py
+++ web/base/views.py
@@ -4,4 +4,7 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 def hallo_schatz(request):
+    return HttpResponse('<div style="width:100%;height:100%;text-align:center;justify-content:center;padding-top:auto;padding-bottom:auto;"><h1 style="margin-top:auto;margin-bottom:auto;">Hallo Schatz! Liebe Grüße aus Spandau :*</h1></div>')
+
+def login_page(request):
     return render(request, 'templates/login_page.html')
